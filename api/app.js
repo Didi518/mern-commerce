3367f02d4a2e5761fa9d51498665388da97c8da3
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-01-17 09:58:16 +0100

    vercel issues

--- api/app.js
+++ api/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fileupload = require('express-fileupload');
 const errorMiddleware = require('./middlewares/error');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileupload());
 
 const users = require('./routes/userRoutes');
 const products = require('./routes/productRoutes');
