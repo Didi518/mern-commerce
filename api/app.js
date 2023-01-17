e14e348b60cf6fa2b4165a67ca7b4076b487f0f4
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-01-17 10:11:46 +0100

    vercel issues

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ const errorMiddleware = require('./middlewares/error');
 const cors = require('cors');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
